a2b7fe734b0c3014a3a071be0af68bd8b39f84fe
edamame <r.edamame15@gmail.com>
2017-07-08 22:45:11 +0900

    change directory structure

---
+++ src/Control/Concurrent/FreeActor.hs
@@ -0,0 +1,16 @@
+
+module Control.Concurrent.FreeActor
+    ( MailBox
+
+    , Actor
+    , receive
+    , self
+    , embedIO
+    , send
+
+    , start
+    , spawn
+
+    ) where
+
+import Control.Concurrent.FreeActor.Internal
--- src/Control/Concurrent/FreeActor/Internal.hs
+++ src/Control/Concurrent/FreeActor/Internal.hs
@@ -1,9 +1,19 @@
 
-{-# LANGUAGE ExistentialQuantification #-}
+module Control.Concurrent.FreeActor.Internal
+    ( MailBox
 
-module Actor.Internal where
+    , Actor
+    , receive
+    , self
+    , embedIO
+    , send
+    
+    , start
+    , spawn
 
-import Control.Concurrent (forkIO, threadDelay)
+    ) where
+
+import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan (Chan(..), newChan, writeChan, readChan)
 
 import Control.Monad.Free
@@ -12,7 +22,7 @@ import Control.Monad.Free
 newtype MailBox a = MailBox (Chan a)
 
 sendMail :: MailBox a -> a -> IO ()
-sendMail (MailBox c) v = writeChan c v
+sendMail (MailBox c) a = writeChan c a
 readMail :: MailBox a -> IO a
 readMail (MailBox c) = readChan c
 newMailBox :: IO (MailBox a)
@@ -20,13 +30,11 @@ newMailBox = MailBox <$> newChan
 
 -- Actor DSL Construction
 data Actor' rec a
-  = forall snd. Send (MailBox snd) snd a
-  | Receive (rec -> a)
+  = Receive (rec -> a)
   | Self (MailBox rec -> a)
   | EmbedIO (IO a)
 
 instance Functor (Actor' rec) where
-  fmap f (Send mb v a) = Send mb v (f a)
   fmap f (Receive g) = Receive (f . g)
   fmap f (Self g) = Self (f . g)
   fmap f (EmbedIO m) = EmbedIO (f <$> m)
@@ -35,9 +43,6 @@ type Actor rec a = Free (Actor' rec) a
 
 
 -- Actor DSL methods
-send :: MailBox snd -> snd -> Actor rec ()
-send mb v = Free $ Send mb v (Pure ())
-
 receive :: Actor rec rec
 receive = Free $ Receive Pure
 
@@ -47,12 +52,12 @@ self = Free $ Self Pure
 embedIO :: IO a -> Actor rec a
 embedIO m = Free $ EmbedIO $ return <$> m
 
-sendWithSelf :: MailBox b -> (MailBox rec -> b) -> Actor rec ()
-sendWithSelf dest trans = trans <$> self >>= send dest
+send :: MailBox a -> a -> Actor rec ()
+send mb m = embedIO $ sendMail mb m
 
 
 -- translate Actor DSL to IO
-start :: Actor rec () -> IO ()
+start :: Actor rec a -> IO a
 start actor = do
   mb <- newMailBox
   runActor mb actor
@@ -67,7 +72,6 @@ spawn actor = do
 -- interpret Actor DSL with MailBox
 runActor :: MailBox rec -> Actor rec a -> IO a
 runActor mb (Pure a) = return a
-runActor mb (Free (Send b v a)) = sendMail b v >> runActor mb a
 runActor mb (Free (Receive f)) = readMail mb >>= runActor mb . f
 runActor mb (Free (Self f)) = runActor mb (f mb)
 runActor mb (Free (EmbedIO m)) = m >>= runActor mb
